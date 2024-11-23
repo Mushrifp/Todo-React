ee9787233c05f755e8795cef07f918f18528c72d
Muhammed Mushrif <mushrifmuhammad6@gmail.com>
2024-11-23 22:56:56 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Todo from './components/todo/todo'
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1></h1>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/todo/todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const toDo = () => {
+  return (
+    <div>
+
+        <h2>To Do</h2>
+        <input type="text"  />
+    </div>
+  )
+}
+
+export default toDo
